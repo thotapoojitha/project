cbe351160f0189e3d9822d1320b0c103d8ddd397
thotapoojitha <108353435+thotapoojitha@users.noreply.github.com>
2024-06-19 23:21:30 +0530

    Update settings.py

--- M1/settings.py
+++ M1/settings.py
@@ -25,7 +25,7 @@ SECRET_KEY = '+9_9(3qpzpuzpz9%z28eg9jvj5ux6dr$4qjgkxl)=r$&68cmwz'
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ['pnsvd.pythonanywhere.com']
 
 
 # Application definition
